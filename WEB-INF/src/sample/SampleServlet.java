444d22c3a3db9b2801eba5e1d84bad1bb7dd4fbd
jins <jins@PC-5-63>
2017-01-13 16:12:21 +0900

    1

--- WEB-INF/src/sample/SampleServlet.java
+++ WEB-INF/src/sample/SampleServlet.java
@@ -2,6 +2,7 @@ package sample;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -26,14 +27,35 @@ public class SampleServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		OutputStream os = resp.getOutputStream();
-		os.write("test".getBytes());
-		os.flush();
+		HashMap<String, String> address = new HashMap<String, String>(){
+			{put("4000858", "<br>山梨県甲府市相生</br>");}
+			{put("4000867", "山梨県甲府市青沼");}
+			{put("4000862", "山梨県甲府市朝気");}
+			{put("4000023", "山梨県甲府市愛宕町");}
+			{put("4000061", "山梨県甲府市荒川");}
+		};
 
 
-		//resp.setContentType("application/json;charset=UTF-8");
+		resp.setContentType("application/text;charset=UTF-8");
 
+		String ad = req.getParameter("zipcode");
 
+		boolean ch = false;
+		for(String key : address.keySet()) {
+			if(key.startsWith(ad)){
+				//System.out.println(address.get(key));
+				OutputStream os = resp.getOutputStream();
+				os.write(address.get(key).getBytes());
+				os.flush();
+				ch = true;
+				//break;
+			}
 
+	    }
+		if(!ch){
+			OutputStream os = resp.getOutputStream();
+			os.write("該当する住所はありません｡".getBytes());
+			os.flush();
+		}
 	}
 }
